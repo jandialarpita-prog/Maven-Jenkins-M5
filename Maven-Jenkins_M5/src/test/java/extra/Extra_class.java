ccf2886ee0c8452cfcdcf7a7947003c1f757dd34
hp <hp@DESKTOP-J8AE8VB>
2025-11-23 17:19:47 +0530

    extra

--- Maven-Jenkins_M5/src/test/java/extra/Extra_class.java
+++ Maven-Jenkins_M5/src/test/java/extra/Extra_class.java
@@ -1,5 +1,16 @@
 package extra;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Extra_class {
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+	}
+    
 
 }
